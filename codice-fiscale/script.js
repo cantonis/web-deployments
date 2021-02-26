aaeb699e8fd37a52616c8acb5abca10e9e4a2838
Simone Cantoni <71756284+cantonis@users.noreply.github.com>
2021-02-26 10:47:17 +0100

    Uso solo di stringhe, eliminazione di array

--- codice-fiscale/script.js
+++ codice-fiscale/script.js
@@ -110,21 +110,18 @@ function bordoLuogo(e) {
  * @returns {string}
  */
 function calcolaCognome(c) {
-	const VOCALI = ["A", "E", "I", "O", "U"];
-	const CONSONANTI = [
-		"B", "C", "D", "F", "G", "H", "J", "K", "L", "M",
-		"N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"
-	]
+	const VOCALI = "AEIOU";
+	const CONSONANTI = "BCDFGHJKLMNPQRSTVWXYZ";
 
 	let cognome = "";
-	let consonantiCognome = new Array();
-	let vocaliCognome = new Array();
+	let consonantiCognome = "";
+	let vocaliCognome = "";
 
 	for (let i = 0; i < c.length; i++) {
 		const char = c.charAt(i);
 
-		if (CONSONANTI.includes(char)) consonantiCognome.push(char);
-		else if (VOCALI.includes(char)) vocaliCognome.push(char);
+		if (CONSONANTI.includes(char)) consonantiCognome += char;
+		else if (VOCALI.includes(char)) vocaliCognome += char;
 	}
 
 	for (let i = 0; i < consonantiCognome.length && cognome.length < 3; i++)
@@ -141,21 +138,18 @@ function calcolaCognome(c) {
  * @returns {string}
  */
 function calcolaNome(n) {
-	const VOCALI = ["A", "E", "I", "O", "U"];
-	const CONSONANTI = [
-		"B", "C", "D", "F", "G", "H", "J", "K", "L", "M",
-		"N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"
-	]
+	const VOCALI = "AEIOU";
+	const CONSONANTI = "BCDFGHJKLMNPQRSTVWXYZ";
 
 	let nome = "";
-	let consonantiNome = new Array();
-	let vocaliNome = new Array();
+	let consonantiNome = "";
+	let vocaliNome = "";
 
 	for (let i = 0; i < n.length; i++) {
 		const char = n.charAt(i);
 
-		if (CONSONANTI.includes(char)) consonantiNome.push(char);
-		else if (VOCALI.includes(char)) vocaliNome.push(char);
+		if (CONSONANTI.includes(char)) consonantiNome += char;
+		else if (VOCALI.includes(char)) vocaliNome += char;
 	}
 
 	for (let i = 0; i < consonantiNome.length && nome.length < 3; i++) {
@@ -175,7 +169,7 @@ function calcolaNome(n) {
  */
 function calcolaData(a, m) {
 	let data = a.substring(2);
-	const MESI = [null, "A", "B", "C", "D", "E", "H", "L", "M", "P", "R", "S", "T"];
+	const MESI = " ABCDEHLMPRST";
 	data += MESI[m];
 
 	return data;
@@ -201,17 +195,14 @@ function calcolaControllo(c) {
 		"9": 21, "A": 1, "B": 0, "C": 5, "D": 7, "E": 9, "F": 13, "G": 15, "H": 17,
 		"I": 19, "J": 21, "K": 2, "L": 4, "M": 18, "N": 20, "O": 11, "P": 3, "Q": 6,
 		"R": 8, "S": 12, "T": 14, "U": 16, "V": 10, "W": 22, "X": 25, "Y": 24, "Z": 23
-	}
+	};
 	const CHARPARI = {
 		"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
 		"9": 9, "A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7,
 		"I": 8, "J": 9, "K": 10, "L": 11, "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16,
 		"R": 17, "S": 18, "T": 19, "U": 20, "V": 21, "W": 22, "X": 23, "Y": 24, "Z": 25
-	}
-	const CHARRESTI = [
-		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
-		"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-	]
+	};
+	const CHARRESTI = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	let sommaPari = 0;
 	for (let i = 1; i < c.length; i += 2) {
