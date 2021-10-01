93ad9771952f837fcd494b277de05293673b9715
Simone Cantoni <71756284+cantooo@users.noreply.github.com>
2021-10-01 09:13:15 +0200

    Password generata ora è di quella lunghezza

--- passwd-gen/script.js
+++ passwd-gen/script.js
@@ -1,18 +1,18 @@
 function passwdGen() {
 	let passwdLength = document.getElementById("passwdLength");
 
-	if (passwdLength.value == "") { passwdLength.style.borderColor = "red"; return; }
+	if (passwdLength.value == "" || passwdLength.value <= 0) { passwdLength.style.borderColor = "red"; return; }
 	else { passwdLength.style.borderColor = ""; passwdLength = passwdLength.value }
 
 	const dict = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 	let passwd = "";
 
-	for (let i = 0; i <= passwdLength; i++) {
+	for (let i = 0; i < passwdLength; i++) {
 		let pos = Math.floor(Math.random() * dict.length);
 		passwd += dict[pos];
 
-		if (i % 5 == 0 && i != 0 && i != passwdLength) passwd += "-";
+		if (i % 5 == 0 && i != 0 && i != passwdLength) { passwd += "-"; passwdLength--; };
 	}
 
 	console.log(passwd)
